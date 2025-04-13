4492d4bfe1ccb5e85ebf72917a728672d320657f
ALLEN-SandeepKumarMurmu <sandeep.murmu@allen.in>
2025-04-13 22:43:50 +0530

    updates

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
   withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
